204484a1202920b673756efeb20002bfb0356d32
Trung Le <trungle@trungs-mac-mini.net.fpt>
2017-06-05 10:29:30 +0700

    copy from template and del unneceesary things

--- TrungCalculator/ViewController.swift
+++ TrungCalculator/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+    
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
